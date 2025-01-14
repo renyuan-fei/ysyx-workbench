3e81de4d18bcc67289b912e6049c396dfe3a4ee3
Fei Renyuan <renyuan-fei@users.noreply.github.com>
2025-01-14 23:48:23 +0800

    verilator env config completed, sim success, makefile completed

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <verilated.h>
+#include "verilated_vcd_c.h"
+#include "../obj_dir/Vtop.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+#define MAX_TIME 20
+vluint64_t sim_time = 0;
+
+int main(int argc, const char** argv) {
+  Vtop *vtop = new Vtop;
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *m_trace = new VerilatedVcdC;
+  vtop->trace(m_trace, 5);
+  m_trace->open("waveformd.vcd");
+  while (sim_time<MAX_TIME)
+  {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    vtop->a = a;
+    vtop->b = b;
+    vtop->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, vtop->f);
+    assert(vtop->f == (a ^ b));
+    m_trace->dump(sim_time);
+    sim_time++;
+  }
+  m_trace->close();
+  delete vtop;
+  exit(EXIT_SUCCESS);
 }
